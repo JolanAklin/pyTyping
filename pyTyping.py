27b2f7fb056568ec1af0d507e24351e1c68be7cb
Jolan Aklin <jolanaklin@gmail.com>
2020-10-07 13:51:58 +0200

    Show if the letter was right or wrong with a color

--- pyTyping.py
+++ pyTyping.py
@@ -10,7 +10,9 @@ shownLetter = 0
 rightLetter = 0
 allTypedLetter = 0
 
+#this function is called when a letter is entered in the textbox. It's verifying if the letter is right or wrong
 def text_inserted(label):
+    letterDisplay.set_name("letterDisplay")
     tbx = builder.get_object('tbx')
     tbxText=tbx.get_text()
     if(tbxText != ""):
@@ -20,44 +22,49 @@ def text_inserted(label):
         label.set_text(tbx.get_text())
         tbx.set_text("")
         if(tbxText == randLetter):
+            letterDisplay.set_name("letterDisplayRight")
             global rightLetter
             rightLetter += 1
             lblRightLetter.set_text('right letter: ' + str(rightLetter))
             changeLetter()
+        else:
+            letterDisplay.set_name("letterDisplayError")
  
 
+#pick a letter randomly
 def randomLetter(letterArray):
     randint = random.randint(0,len(letterArray)-1)
     return letterArray[randint]
 
-
+#change the letter on the display
 def changeLetter():
     global randLetter
     global shownLetter
     shownLetter += 1
     lblTotLetter.set_text('shown letter: ' + str(shownLetter))
     randLetter = randomLetter(letterArray)
-    letterDisplay.set_markup('<span background="blue">'+str(randLetter)+'</span>')
-    #letterDisplay.set_text(randLetter)
+    letterDisplay.set_markup(str(randLetter))
     
-
+#load the css stylesheet
 screen = Gdk.Screen.get_default()
 
 css_provider = Gtk.CssProvider()
-css_provider.load_from_path('test.css')
+css_provider.load_from_path('style.css')
 
 context = Gtk.StyleContext()
 context.add_provider_for_screen(screen, css_provider,
   Gtk.STYLE_PROVIDER_PRIORITY_USER)
 
-letterArray = ['a', 'b', 'c', 'd', 'e', 'f']
+#letter that while be random picked
+letterArray = ['q', 'w', 'e', 'r', 't', 'z', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'y', 'x', 'c', 'v', 'b', 'n', 'm']
+
 builder = Gtk.Builder()
-builder.add_from_file('testui.glade')  # Rentrez évidemment votre fichier, pas le miens!
+builder.add_from_file('testui.glade')
 
 window = builder.get_object('main_window')
-# Peut se faire dans Glade mais je préfère le faire ici, à vous de voir
 window.connect('delete-event', Gtk.main_quit)
 
+#defining globally the text counter
 global lblTotLetter
 lblTotLetter = builder.get_object('lblTotLetter')
 global lblRIghtLetter
@@ -70,7 +77,7 @@ letterDisplay = builder.get_object('letterDisplay')
 changeLetter()
 
 
-# Le handler
+#event handler
 handler = {'on_insert_text': text_inserted}
 builder.connect_signals(handler)
 
